3ba6ec673767e1f4d7c249713710cd0c487d82e7
Rupam Mondal <rupam8906@gmail.com>
2025-04-10 00:03:08 +0530

    fix: correct error message extraction in errorMessage utility function

--- src/utils/errorMessage.js
+++ src/utils/errorMessage.js
@@ -1,9 +1,9 @@
 export default function errorMessage(error){
-    const htmlString = error?.response?.data
+    const htmlString = error?.response?.data?.message
     const match = htmlString?.match(/Error:.*?(?=<br>)/);
     if(match){
         return(match[0].slice(7).replaceAll("&quot;", `"`)) 
     }else{
-        return(error.message)
+        return(error.response?.data?.message)
     } 
 }
